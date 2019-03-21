ec9d04d9278520803f9a4a481c37f77e371b2acc
longarms07 <ecridd@gmail.com>
2019-03-21 16:04:29 -0400

    Beginning of tests
    Did some initial tests. The code is currently non-functional, we're gonna need to actually go to whatever website we want to use (I used github because I had an example from it that I borrowed the secret key from for testing, I don't plan to use it for the final product.) I have a general idea of what to do moving forward though

--- gradGyde/__init__.py
+++ gradGyde/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
 app = Flask("gradGyde")
+app.config.from_pyfile('config.py')
+secret_key = app.config['SECRET_KEY']
 
 from . import main
--- gradGyde/main.py
+++ gradGyde/main.py
@@ -1,4 +1,23 @@
 from gradGyde import app
+from flask import request, session, url_for
+from flask_oauthlib.client import OAuth, redirect
+
+oauth = OAuth()
+github = oauth.remote_app('github',
+	consumer_key='2', #We need to register and obtain our own
+    consumer_secret='2', 
+    request_token_params={'scope': 'user:email'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+	authorize_url='https://github.com/login/oauth/authorize'
+	)
+
+@github.tokengetter
+def get_github_token(token=None):
+	return session.get("github_token")
+
 
 @app.route('/')
 def index():
@@ -7,3 +26,28 @@ def index():
 @app.route('/test')
 def test():
     return "test"
+
+@app.route('/oauth_test')
+def oauth_test():
+	return github.authorize(callback=url_for('oauth_github_authorized', 
+		next =request.args.get('next') or request.referrer or None))
+
+@app.route('/oauth_github_authorized')
+def oauth_github_authorized():
+	next_url = request.args.get('next') or url_for('oauth_sucess')
+	resp = github.authorized_response()
+	if resp is None:
+		flash(u'You denied the request to sign in.')
+		print('You denied the request to sign in.')
+		return redirect(next_url)
+	session['github_token'] = (
+		resp['oauth_token'],
+		resp['oauth_token_secret'])
+	session['github_user']=resp['screen_name']
+	flash(u'You were signed in as %/' % resp['screen_name'])
+	print('You were signed in as %/' % resp['screen_name'])
+	return redirect('/oauth_test_success')
+
+@app.route('/oauth_test_success')
+def oauth_sucess():
+	return "%/ Successfully logged in! Yaaaay!" % resp['screen_name']
