af0aa675b0d6b8cb820c41ee967726b5f67f0b5d
longarms07 <ecridd@gmail.com>
2019-03-23 19:41:48 -0400

    Removed temp config file

--- gradGyde/config.py
+++
@@ -1,4 +0,0 @@
-#This is a temporary config for testing purposes. 
-#Ideally the real app will have one of these protected by a gitignore that gets put into the docker image, I don't want to upload our secret key online again
-
-SECRET_KEY = "this is just for a test we will need to make a real one and config file later"
