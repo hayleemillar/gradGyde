33addb6b09451ca4e91ebfc23ad57c79691f9f18
jordan-deagan <jdeags@gmail.com>
2019-04-07 17:44:35 -0400

    potential fixes for travis

--- gradGyde/db_helper.py
+++ gradGyde/db_helper.py
@@ -1,180 +1,194 @@
 import datetime
 from gradGyde import db
-from .models import*
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from .models import *
+
+
+engine = create_engine('sqlite:///gradGyde.db')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class DatabaseHelper():
-    def make_aoc(self, name, passed_type, year):
+
+    @classmethod
+    def make_aoc(cls, name, passed_type, year):
         try:
             new_aoc = Aocs(aoc_name=name,
                            aoc_type=passed_type,
                            aoc_year=year)
-            db.session.add(new_aoc)
-            db.session.commit()
+            session.add(new_aoc)
+            session.commit()
         except Exception as error:
             raise Exception("Could not create AOC! " + str(error))
 
-    def make_class(self, name, semester, year, credit):
+    @classmethod
+    def make_class(cls, name, semester, year, credit):
         try:
             new_class = Classes(class_name=name,
                                 class_semester=semester,
                                 class_year=year,
                                 credit_type=credit)
-            db.session.add(new_class)
-            db.session.commit()
+            session.add(new_class)
+            session.commit()
         except Exception as error:
             raise Exception("Could not create Class! " + str(error))
 
-    def make_user(self, email, name, year, u_type):
+    @classmethod
+    def make_user(cls, email, name, year, u_type):
         try:
-            if self.get_user(email) is None:
+            if cls.get_user(email) is None:
                 new_user = Users(user_email=email,
                                  user_name=name,
                                  year_started=year,
                                  user_type=u_type)
-                db.session.add(new_user)
-                db.session.commit()
+                session.add(new_user)
+                session.commit()
         except Exception as error:
             raise Exception("Could not create User! " + str(error))
 
-    def make_tag(self, name):
+    @classmethod
+    def make_tag(cls, name):
         try:
             new_tag = Tags(tag_name=name)
-            db.session.add(new_tag)
-            db.session.commit()
+            session.add(new_tag)
+            session.commit()
         except Exception as error:
             raise Exception("Could not create Tag! " + str(error))
 
-    def make_requirement(self, aoc, tag, required):
+    @classmethod
+    def make_requirement(cls, aoc, tag, required):
         try:
             new_req = Requirements(aoc_id=aoc.aoc_id,
                                    tag_id=tag.tag_id,
                                    num_req=required)
-            db.session.add(new_req)
-            db.session.commit()
+            session.add(new_req)
+            session.commit()
         except Exception as error:
             raise Exception("Could not create Requirement! " + str(error))
 
-    def assign_prereqs(self, prereq, chosen):
+    @classmethod
+    def assign_prereqs(cls, prereq, chosen):
         try:
             new_prereq = Prereqs(prereq_tag_id=prereq.tag_id,
                                  chosen_tag_id=chosen.tag_id)
-            db.session.add(new_prereq)
-            db.session.commit()
+            session.add(new_prereq)
+            session.commit()
         except Exception as error:
             raise Exception("Could not assign Prereq! " + str(error))
 
-    def assign_tags(self, class_assigned, tag):
+    @classmethod
+    def assign_tags(cls, class_assigned, tag):
         try:
             new_type = ClassTags(class_id=class_assigned.class_id,
                                  tag_id=tag.tag_id)
-            db.session.add(new_type)
-            db.session.commit()
+            session.add(new_type)
+            session.commit()
         except Exception as error:
             raise Exception("Could not assign Tag! " + str(error))
 
-    def assign_aoc(self, aoc, user):
+    @classmethod
+    def assign_aoc(cls, aoc, user):
         try:
             new_major = PrefferedAocs(aoc_id=aoc.aoc_id,
                                       user_id=user.user_id)
-            db.session.add(new_major)
-            db.session.commit()
+            session.add(new_major)
+            session.commit()
         except Exception as error:
             raise Exception("Could not assign AOC! " + str(error))
 
-    def take_class(self, class_taken, student):
+    @classmethod
+    def take_class(cls, class_taken, student):
         try:
             new_pass = ClassTaken(student_id=student.user_id,
                                   class_id=class_taken.class_id)
-            db.session.add(new_pass)
-            db.session.commit()
+            session.add(new_pass)
+            session.commit()
         except Exception as error:
             raise Exception("Could not take Class! " + str(error))
 
-    def create_class(self, self, name, semester, year, credit, tags):
-        self.make_class(name, semester, year, credit)
-        created_class = Classes.query.filter_by(class_name=name
-                                                ).filter_by(class_semester=semester
-                                                ).filter_by(class_year=year
-                                                ).filter_by(credit_type=credit
-                                                ).first()
+    @classmethod
+    def create_class(cls, name, semester, year, credit, tags):
+        cls.make_class(name, semester, year, credit)
+        created_class = Classes.query.filter_by(class_name=name).filter_by(class_semester=semester).filter_by(class_year=year).filter_by(credit_type=credit).first()
         for tag in tags:
-            da_tag = self.get_tag(tag)
+            da_tag = cls.get_tag(tag)
             if da_tag is not None:
-                self.assign_tags(created_class, da_tag)
+                cls.assign_tags(created_class, da_tag)
 
-    def create_aoc(self, self, name, passed_type, year, tags, amounts):
+    @classmethod
+    def create_aoc(cls, name, passed_type, year, tags, amounts):
         #Tags and Amounts are lists
         try:
-            self.make_aoc(name, passed_type, year)
-            aoc = self.get_aoc(name, passed_type)
+            cls.make_aoc(name, passed_type, year)
+            aoc = cls.get_aoc(name, passed_type)
             for i in range(len(tags)):
-                self.make_tag(tags[i])
-                da_tag = self.get_tag(tags[i])
-                self.make_requirement(aoc, da_tag, amounts[i])
+                cls.make_tag(tags[i])
+                da_tag = cls.get_tag(tags[i])
+                cls.make_requirement(aoc, da_tag, amounts[i])
 
         except Exception as error:
             raise Exception("Could not create AOC! "+str(error))
 
-    def get_user(self, email):
-        user_query = Users.query.filter_by(user_email=email
-                                           ).first()
+    @classmethod
+    def get_user(cls, email):
+        user_query = Users.query.filter_by(user_email=email).first()
         return user_query
 
-    def get_aoc(self, name, passed_type, year=datetime.date.today().year):
-        aoc_query = Aocs.query.filter_by(aoc_name=name
-                                         ).filter_by(aoc_type=passed_type
-                                         ).filter(Aocs.aoc_year <= year
-                                         ).first()
+    @classmethod
+    def get_aoc(cls, name, passed_type, year=datetime.date.today().year):
+        aoc_query = Aocs.query.filter_by(aoc_name=name).filter_by(aoc_type=passed_type).filter(Aocs.aoc_year <= year).first()
         return aoc_query
 
-    def get_aoc_by_id(self, a_id):
-        aoc_query = Aocs.query.filter_by(aoc_id=a_id
-                                         ).first()
+    @classmethod
+    def get_aoc_by_id(cls, a_id):
+        aoc_query = Aocs.query.filter_by(aoc_id=a_id).first()
         return aoc_query
 
-    def get_aocs_by_type(self, pref_type):
+    @classmethod
+    def get_aocs_by_type(cls, pref_type):
         aocs_query = Aocs.query.filter_by(aoc_type=pref_type).all()
         return aocs_query
 
-    def get_preffered_aocs(self, user, pref_type):
+    @classmethod
+    def get_preffered_aocs(cls, user, pref_type):
         pref_aocs_query = PrefferedAocs.query.filter_by(user_id=user.user_id).all()
         pref_aocs = []
         #add check for size and pass general studies if there are no prefered aocs
         for item in pref_aocs_query:
-            aoc = self.get_aoc_by_id(item.aoc_id)
+            aoc = cls.get_aoc_by_id(item.aoc_id)
             if aoc is not None and aoc.aoc_type == pref_type:
                 pref_aocs.append(aoc)
         return pref_aocs
 
-    def get_class(self, c_name):
-        class_query = Classes.query.filter_by(class_name=c_name
-                                              ).first()
+    @classmethod
+    def get_class(cls, c_name):
+        class_query = Classes.query.filter_by(class_name=c_name).first()
         return class_query
 
-    def get_class_by_id(self, c_id):
-        class_query = Classes.query.filter_by(class_id=c_id
-                                              ).first()
+    @classmethod
+    def get_class_by_id(cls, c_id):
+        class_query = Classes.query.filter_by(class_id=c_id).first()
         return class_query
 
-    def get_classes_taken(self, user):
-        class_taken_query = ClassTaken.query.filter_by(student_id=user.user_id
-                                                     ).all()
+    @classmethod
+    def get_classes_taken(cls, user):
+        class_taken_query = ClassTaken.query.filter_by(student_id=user.user_id).all()
         classes_taken = []
         for class_taken in class_taken_query:
-            da_class = self.get_class_by_id(class_taken.class_id)
+            da_class = cls.get_class_by_id(class_taken.class_id)
             if da_class is not None:
                 classes_taken.append(da_class)
         return classes_taken
 
-
-    def get_tag(self, text):
-        tag_query = Tags.query.filter_by(tag_name=text
-                                       ).first()
+    @classmethod
+    def get_tag(cls, text):
+        tag_query = Tags.query.filter_by(tag_name=text).first()
         return tag_query
 
-    def get_tag_by_id(self, t_id):
-        tag_query = Tags.query.filter_by(tag_id=t_id
-                                       ).first()
+    @classmethod
+    def get_tag_by_id(cls, t_id):
+        tag_query = Tags.query.filter_by(tag_id=t_id).first()
         return tag_query
 
 
@@ -218,8 +232,7 @@ class DatabaseHelper():
         for tag in tags:
             print(self.get_tag(tag))
         #Test class
-        self.create_class("Introduction to Programming With Python",
-                          SemesterType.FALL, 2018, 1, [tags[0]])
+        self.create_class("Introduction to Programming With Python", SemesterType.FALL, 2018, 1, [tags[0]])
         da_class = self.get_class("Introduction to Programming With Python")
         print(da_class)
         #Test user
