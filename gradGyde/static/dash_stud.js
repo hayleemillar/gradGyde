dc613ced772f1c5fa1db74ec7b642b5c81707a27
hayleemillar <11101998Brad>
2019-04-15 06:42:13 -0400

    starting work on javascript using json example provided by backend

---
+++ gradGyde/static/dash_stud.js
@@ -0,0 +1,41 @@
+aocs = {
+  AOC1 : {
+    Name : "Computer Science 2018",
+      Requirements : {
+        Req1 : {
+          Name :  "CS Introductory Course"
+          Ammount : 1
+          Fullfilled : true
+          Classes : {
+            Class1 : {
+              Name : "Intro to Programming in Python",
+              Taken : false
+            }
+            Class2 : {
+              Name : "Intro to Programming in C",
+              Taken : true
+            }   
+          }   
+        }
+        Req2 : {
+          Name :  "Math"
+          Ammount : 2
+          Fullfilled : false
+          Classes : {
+            Class1 : {
+              Name : "Calculus 1",
+              Taken : false
+            }
+            Class2 : {
+              Name : "Discrete Mathematics for Computer Science",
+              Taken : true
+            }
+            Class3 : {
+              Name : "Dealing With Data",
+              Taken : false
+            }   
+          }   
+        }
+      }
+    }
+  };
